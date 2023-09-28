5f211ec45b60beb93340eff294a632fd1944f200
xiaobin.Guo <xiaobin.guo@s-navimentum.com>
2023-09-28 08:51:45 +0800

    Lot制程结束逻辑开发

--- src/main/java/com/totainfo/eap/cp/service/kvm/KVMAlarmReportService.java
+++ src/main/java/com/totainfo/eap/cp/service/kvm/KVMAlarmReportService.java
@@ -1,14 +1,20 @@
 package com.totainfo.eap.cp.service.kvm;
 
 import com.totainfo.eap.cp.base.service.EapBaseService;
+import com.totainfo.eap.cp.dao.ILotDao;
+import com.totainfo.eap.cp.entity.LotInfo;
 import com.totainfo.eap.cp.handler.ClientHandler;
 import com.totainfo.eap.cp.handler.MesHandler;
 import com.totainfo.eap.cp.trx.kvm.KVMAlarmReport.KVMAlarmReportI;
 import com.totainfo.eap.cp.trx.kvm.KVMAlarmReport.KVMAlarmReportO;
 import com.totainfo.eap.cp.trx.mes.EAPEqptAlarmReport.EAPEqptAlarmReportI;
+import com.totainfo.eap.cp.trx.mes.EAPReqCheckOut.EAPReqCheckOutO;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.awt.*;
+
+import static com.totainfo.eap.cp.commdef.GenergicCodeDef.LOT_INFO_EXIST;
 
 /**
  * @author xiaobin.Guo
@@ -17,6 +23,9 @@ import java.awt.*;
 @Service("KVMAlarmReporting")
 public class KVMAlarmReportService extends EapBaseService<KVMAlarmReportI, KVMAlarmReportO> {
 
+    @Resource
+    private ILotDao lotDao;
+
     @Override
     public void mainProc(String evtNo, KVMAlarmReportI inTrx, KVMAlarmReportO outTrx) {
         String alarmCode = inTrx.getAlarmCode();
@@ -24,8 +33,24 @@ public class KVMAlarmReportService extends EapBaseService<KVMAlarmReportI, KVMAl
         String time = inTrx.getTime();
         MesHandler.alarmReport(evtNo, alarmCode, alarmText, time);
 
-        //todo 报警需要推送给Client端展示
+        //报警需要推送给Client端展示
         ClientHandler.sendMessage(evtNo, false, 2, "设备发送报警:[" + alarmCode + "]["+ alarmText +"]");
+
+        //判断是不是制程结束的报警，如果是，发给MES Check Out
+        if(!"00405".equals(alarmCode)){
+             return;
+        }
+
+        LotInfo lotInfo = lotDao.getCurLotInfo();
+        if(lotInfo != null){
+            outTrx.setRtnCode(LOT_INFO_EXIST);
+            outTrx.setRtnMesg("批次:[" + lotInfo.getLotId() + "]制程未结束，请等待");
+            return;
+        }
+
+        EAPReqCheckOutO eapReqCheckOutO = MesHandler.checkOutReq(evtNo, lotInfo.getLotId());
+        outTrx.setRtnCode(eapReqCheckOutO.getRtnCode());
+        outTrx.setRtnMesg(eapReqCheckOutO.getRtnMesg());
 
     }
 }
