c951ffe949fd544e2d59ac9b63013204ca23a16f
xiaobin.Guo <xiaobin.guo@s-navimentum.com>
2024-02-26 10:22:11 +0800

    修改AOP

--- src/main/java/com/totainfo/eap/cp/aop/LogAcpect.java
+++ src/main/java/com/totainfo/eap/cp/aop/LogAcpect.java
@@ -1,5 +1,6 @@
 package com.totainfo.eap.cp.aop;
 
+
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.totainfo.eap.cp.handler.ClientHandler;
 import com.totainfo.eap.cp.util.JacksonUtils;
@@ -8,35 +9,40 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
-
-
 import static com.totainfo.eap.cp.commdef.GenergicStatDef.Constant.RETURN_CODE_OK;
 
 
 @Aspect
 @Component
-public class KVMAop {
+public class LogAcpect {
+
 
 
     /**
      * 定义切入点，切入点为com.example.aop下的所有函数
      */
-    @Pointcut("execution(* com.totainfo.eap.cp.service.kvm.*.subMainProc(..))")
-    public void webLog(){}
+    @Pointcut("execution(* com.totainfo.eap.cp.base.service.EapBaseService.subMainProc(..))")
+    public void eapLog(){}
 
+    /**
+     * 前置通知：在连接点之前执行的通知
+     * @param joinPoint
+     * @throws Throwable
+     */
 
-    @AfterReturning(returning = "ret",pointcut = "webLog()")
+    @AfterReturning(returning = "ret",pointcut = "eapLog()")
     public void doAfterReturning(JoinPoint joinPoint, Object ret) throws Throwable {
         if (ret == null) {
             return;
         }
-        Object[] args = joinPoint.getArgs();
-        String evtNo = args[0].toString();
-        ObjectNode objectNode = JacksonUtils.getJson2(ret.toString());
+        Object[] objects = joinPoint.getArgs();
+        String outTrxMsg = ret.toString();
+
+        ObjectNode objectNode = JacksonUtils.getJson2(outTrxMsg);
         String rtnCode = objectNode.get("rtnCode").textValue();
         String rtnMesg = objectNode.get("rtnMesg").textValue();
         if(!RETURN_CODE_OK.equals(rtnCode)){
-            ClientHandler.sendMessage(evtNo, true, 1, rtnMesg);
+            ClientHandler.sendMessage(objects[0].toString(),false,2,rtnMesg);
         }
     }
 }
---
+++ src/test/java/FtpTest.java
@@ -0,0 +1,55 @@
+import com.totainfo.eap.cp.util.FtpUtils;
+import com.totainfo.eap.cp.util.LogUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * @author xiaobin.Guo
+ * @date 2024年01月30日 9:12
+ */
+public class FtpTest {
+
+    public void test(){
+        Path dataPath = Paths.get(System.getProperty("user.dir")+ File.separator+"tmp", "data","MES_DATA" + ".TXT");
+        if (!Files.exists(dataPath.getParent())){
+            try {
+                Files.createDirectories(dataPath.getParent());
+            } catch (IOException e) {
+                LogUtils.error("文件夹创建失败");
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("TP_NAME:").append("123").append("\n")
+                .append("TESTER_ID:").append("").append("\n")
+                .append("PART_NO:").append("").append("\n")
+                .append("QTY:").append("").append("\n")
+                .append("C_LOT:").append("").append("\n")
+                .append("HANDLER:").append("NA").append("\n")
+                .append("PROBER:").append("NA").append("\n")
+                .append("LOADBOARD:").append("NA").append("\n")
+                .append("PROBERCARD:").append("456").append("\n")
+                .append("SOCKET:").append("NA").append("\n")
+                .append("PROCESS:").append("").append("\n")
+                .append("STEP:").append("").append("\n")
+                .append("RETEST:").append("NA").append("\n")
+                .append("RT_BIN:").append("NA").append("\n")
+                .append("SUBCON_NAME:").append("").append("\n")
+                .append("SUBCON_LOT:").append("").append("\n")
+                .append("DATE_CODE:").append("NA").append("\n")
+                .append("OPID:").append("").append("\n")
+                .append("PASS_BIN:").append("").append("\n")
+                .append("WORK_MODE:").append("").append("\n")
+                .append("EXTENSION:").append("").append("\n");
+        try {
+            Files.write(dataPath, sb.toString().getBytes());
+            FtpUtils.uploadFile("127.0.0.1", "root", "123", 21, "C:\\",dataPath.toString());
+            Files.delete(dataPath);
+        } catch (IOException e) {
+            LogUtils.error("xtr文件写入失败");
+        }
+    }
+}
