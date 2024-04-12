dde72b561a0d40c89f8f4849a7a1b75194a67920
TheWangShuai <2013464103@qq.com>
2024-04-12 13:46:33 +0800

    bug修复

--- src/main/java/com/totainfo/eap/cp/handler/RabbitmqHandler.java
+++ src/main/java/com/totainfo/eap/cp/handler/RabbitmqHandler.java
@@ -53,7 +53,7 @@ public class RabbitmqHandler<I extends BaseTrxI> {
         Message message = new Message(inObjStr.getBytes(), properties);
 
         rabbitTemplate.convertAndSend(exchange, queue, message);
-        LogUtils.info("[{}][{}][{}]:[{}]", evtNo,"EAP->"+appName,inObj.getTrxId(),  inObjStr);
+        LogUtils.info("[{}][{}][{}][{}][{}]:[{}]", evtNo,"EAP->"+appName,"Exchange:"+exchange,"Queue:"+queue,inObj.getTrxId(),  inObjStr);
     }
 
     public String sendForReply(String evtNo, String appName, String queue, String exchange, I inObj) {
@@ -68,7 +68,7 @@ public class RabbitmqHandler<I extends BaseTrxI> {
         properties.getHeaders().put("actionFlag", inObj.getActionFlg());
         String inObjStr = JacksonUtils.object2String(inObj);
         Message message = new Message(inObjStr.getBytes(), properties);
-        LogUtils.info("[{}][{}][{}]:[{}]", evtNo, "EAP->"+appName, trxId,  inObjStr);
+        LogUtils.info("[{}][{}][{}][{}][{}]:[{}]", evtNo,"EAP->"+appName,"Exchange:"+exchange,"Queue:"+queue,inObj.getTrxId(),  inObjStr);
         AsyncRabbitTemplate.RabbitMessageFuture future = asyncRabbitTemplate.sendAndReceive(exchange, queue, message);
         String reply = _SPACE;
         Message rtnMessage = null;
